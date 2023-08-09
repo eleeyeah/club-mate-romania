1b307fe2d39f2f635f115e8fca95645a62593af5
eleeyeah <ilietmks@gmail.com>
2023-08-09 20:23:55 +0200

    logo updated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 'use client';
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import styles from "./navbar.module.css";
@@ -40,9 +41,8 @@ const Navbar = () => {
   return (
     <div className={styles.container}>
       <Link href='/' className={styles.logo}>
-        <h1>Club Mate</h1>
-        
-        <h4>Romania</h4>
+        <Image src="/logo.png" alt="Club Mate" width={120} height={120} />
+        <h1 className={styles.logo-name} >Club Mate Romania</h1>
       </Link>
       <div className={styles.links}>
         {links.map((link) => (
